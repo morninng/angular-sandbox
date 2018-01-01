083880b800ad69d917805470dfb3569d01a43670
cloudliner <cloudliner.jp@gmail.com>
2018-01-01 16:42:55 +0900

    List 2.22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { NgIfComponent } from './ng-if/ng-if.component';
     NgClassComponent,
     NgStyleComponent,
     NgForComponent,
-    NgIfComponent
+    NgIfComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ng-switch/ng-switch.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch',
+  templateUrl: './ng-switch.component.html'
+})
+export class NgSwitchComponent { }
