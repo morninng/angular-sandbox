c48c3b7adaacdb52489eb10bd2307bb9f8fdca93
cloudliner <cloudliner.jp@gmail.com>
2018-01-02 16:08:02 +0900

    List 2.23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { NgSwitchComponent } from './ng-switch/ng-switch.component';
     NgStyleComponent,
     NgForComponent,
     NgIfComponent,
-    NgSwitchComponent
+    NgSwitchComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
