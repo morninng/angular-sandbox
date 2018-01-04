59939f6430505661983dbd679260656e9c3017fc
cloudliner <cloudliner.jp@gmail.com>
2018-01-04 20:21:59 +0900

    List 4.4

--- src/app/ch04/ch04.component.ts
+++ src/app/ch04/ch04.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ch04',
-  template: '<h1 ch04Attr>Hello Componennt</h1>'
+  template: `
+    <h1 ch04Attr>Hello Componennt</h1>
+    <p>{{message}}</p>
+  `
 })
-export class Ch04Component { }
+export class Ch04Component implements OnInit {
+  message: string;
+  constructor() {
+     this.message = 'Hello from constructor.'
+     console.log(this.message);
+  }
+  ngOnInit() {
+
+    this.message = 'Hello from ngOnInit.'
+    console.log(this.message);
+  }
+}
