b31de3d0870d161ef94a726fb7aebf18f1ac6aa3
cloudliner <cloudliner.jp@gmail.com>
2018-01-07 21:22:25 +0900

    List 4.41

--- src/app/animations/animations.module.ts
+++ src/app/animations/animations.module.ts
@@ -3,9 +3,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AnimationsComponent } from './animations.component';
-import { TransitionComponent } from './transition.component';
 import { StateComponent } from './state.component';
 import { State2Component } from './state2.component';
+import { TransitionComponent } from './transition.component';
+import { Transition2Component } from './transition2.component';
 
 @NgModule({
   imports: [
@@ -14,15 +15,17 @@ import { State2Component } from './state2.component';
   ],
   declarations: [
     AnimationsComponent,
-    TransitionComponent,
     StateComponent,
-    State2Component
+    State2Component,
+    TransitionComponent,
+    Transition2Component
   ],
   exports: [
     AnimationsComponent,
-    TransitionComponent,
     StateComponent,
-    State2Component
+    State2Component,
+    TransitionComponent,
+    Transition2Component
   ]
 })
 export class AnimationsModule { }
