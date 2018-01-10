3cdb048eb22a7498da11889a0c35c4f901132d04
cloudliner <cloudliner.jp@gmail.com>
2018-01-10 23:08:57 +0900

    List 4.66

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import { InputOutputModule } from './input-output/input-output.module';
 import { HostDecoratorModule } from './host-decorator/host-decorator.module';
 import { ViewChildModule } from './view-child/view-child.module';
 import { ContentChildModule } from './content-child/content-child.module';
+import { HostSelectorModule } from './host-selector/host-selector.module';
 
 @NgModule({
   imports: [
@@ -62,7 +63,8 @@ import { ContentChildModule } from './content-child/content-child.module';
     InputOutputModule,
     HostDecoratorModule,
     ViewChildModule,
-    ContentChildModule
+    ContentChildModule,
+    HostSelectorModule
   ],
   declarations: [
     AppComponent,
---
+++ src/app/host-selector/host-selector.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { HostSelectorComponent } from './host-selector.component';
+
+@NgModule({
+  declarations: [
+    HostSelectorComponent
+  ],
+  exports: [
+    HostSelectorComponent
+  ]
+})
+export class HostSelectorModule { }
