b0af2c8b21d5e8cb399aab2f246f8410a8c54c18
cloudliner <cloudliner.jp@gmail.com>
2018-01-10 23:14:38 +0900

    List 4.67

--- src/app/host-selector/host-selector.module.ts
+++ src/app/host-selector/host-selector.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { HostSelectorComponent } from './host-selector.component';
+import { HostSelector2Component } from './host-selector2.component';
 
 @NgModule({
   declarations: [
-    HostSelectorComponent
+    HostSelectorComponent,
+    HostSelector2Component
   ],
   exports: [
-    HostSelectorComponent
+    HostSelectorComponent,
+    HostSelector2Component
   ]
 })
 export class HostSelectorModule { }
