8cbdc7f5af2feef1c6d8aeecfa9adfdd52b91bd7
cloudliner <cloudliner.jp@gmail.com>
2018-01-14 17:26:01 +0900

    List 5.26

--- src/app/rxjs/click-event.component.ts
+++ src/app/rxjs/click-event.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
   template: `
     <button id="button1">クリックイベント（イベントドリブン）</button>
     <button id="button2">クリックイベント（ストリーム）</button>
+    <button id="button3">クリックイベント・1秒1回（イベントドリブン）</button>
   `
 })
 export class ClickEventComponent implements OnInit {
@@ -24,6 +25,19 @@ export class ClickEventComponent implements OnInit {
       const buttonClick$ = Observable.fromEvent(buttonElement, 'click');
       buttonClick$.subscribe(ev => {
         console.log('clicked2');
+      })
+    }
+    {
+      var count = 0;
+      const rate = 1000;
+      var lastClick = Date.now() - rate;
+      const buttonElement = document.getElementById('button3');
+      buttonElement.addEventListener('click', event => {
+        if (Date.now() - lastClick >= rate) {
+          count += 1;
+          console.log(`${count} clicked3!!`);
+          lastClick = Date.now();
+        }
       })
     }
   }
