c917f5d55e27230a84fb698ab5fc4e5eab22b4cc
cloudliner <cloudliner.jp@gmail.com>
2018-01-14 21:37:08 +0900

    List 5.33

--- src/app/rxjs/promise.component.ts
+++ src/app/rxjs/promise.component.ts
@@ -4,11 +4,13 @@ import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'app-promise',
   template: `
-    <p><button (click)="click1()">Promise Observable</button> {{message}}</p>
+    <p><button (click)="click1()">Promise Observable</button> {{message1}}</p>
+    <p><button (click)="click2()">Observable Promise</button> {{message2}}</p>
   `
 })
 export class PromiseComponent {
-  message = '';
+  message1 = '';
+  message2 = '';
 
   constructor() { }
 
@@ -22,8 +24,18 @@ export class PromiseComponent {
     observable.subscribe({
       next: (x) => {
         console.log(x);
-        this.message += x;
+        this.message1 += x;
       }
     });
+  }
+
+  click2() {
+    const promise = Observable
+      .of('Angular')
+      .toPromise();
+    promise.then((x) => {
+      console.log(x);
+      this.message2 += x;
+    })
   }
 }
