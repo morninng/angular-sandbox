295736b567ba9db9c1f808124be01fa56557842e
cloudliner <cloudliner.jp@gmail.com>
2018-01-14 17:10:14 +0900

    List 5.25

--- src/app/rxjs/click-event.component.ts
+++ src/app/rxjs/click-event.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-click-event',
   template: `
     <button id="button1">クリックイベント（イベントドリブン）</button>
+    <button id="button2">クリックイベント（ストリーム）</button>
   `
 })
 export class ClickEventComponent implements OnInit {
@@ -15,6 +17,13 @@ export class ClickEventComponent implements OnInit {
       const buttonElement = document.getElementById('button1');
       buttonElement.addEventListener('click', event => {
         console.log('clicked1');
+      })
+    }
+    {
+      const buttonElement = document.getElementById('button2');
+      const buttonClick$ = Observable.fromEvent(buttonElement, 'click');
+      buttonClick$.subscribe(ev => {
+        console.log('clicked2');
       })
     }
   }
