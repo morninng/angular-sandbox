022217d52a55d25885f468d01c74cf6fa4c6f740
cloudliner <cloudliner.jp@gmail.com>
2018-01-17 14:20:45 +0900

    List 5.42

--- src/app/http/http-options-sample.component.ts
+++ src/app/http/http-options-sample.component.ts
@@ -4,24 +4,36 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-http-options-sample',
   template: `
-    <div>{{ list | json }}</div>
+    <div>{{ list1 | json }}</div>
+    <div>{{ list2 | json }}</div>
   `
 })
 export class HttpOptionsSampleComponent implements OnInit {
-  list: string[];
+  list1: string[];
+  list2: string[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    let params  = new HttpParams();
-    let headers = new HttpHeaders();
-    params = params.append('foo', 'buz');
-    headers = headers.append('X-Api-Token', 'token');
-    const options = {
-      params: params,
-      headers: headers
+    {
+      let params  = new HttpParams();
+      let headers = new HttpHeaders();
+      params = params.append('foo', 'buz1');
+      headers = headers.append('X-Api-Token', 'token1');
+      const options = {
+        params: params,
+        headers: headers
+      }
+      this.http.get<string[]>('/assets/data/list.json', options)
+        .subscribe(list => this.list1 = list);
     }
-    this.http.get<string[]>('/assets/data/list.json', options)
-      .subscribe(list => this.list = list);
+    {
+      const options {
+        params: {'foo': 'buz2'},
+        headers: {'X-Api-Token': 'token2'}
+      }
+      this.http.get<string[]>('/assets/data/list.json', options)
+        .subscribe(list => this.list2 = list);
+    }
   }
 }
